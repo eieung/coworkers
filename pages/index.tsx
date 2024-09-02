7698212df032bf0c1e221e4edd4fac02828f753e
binyongbin <kyb2475@gmail.com>
2024-09-02 13:06:18 +0900

    rename: libs 폴더명 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="w-full p-50">
-      프로젝트 세팅
-    </div>
-  );
+  return <></>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const createPxValues = (count: number) => {
   return Object.fromEntries(
-    Array.from({ length: count + 1 }, (_, i) => [`${i}`, `${i}px`])
+    Array.from({ length: count + 1 }, (_, i) => [`${i}`, `${i}px`]),
   );
 };
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -26,64 +26,70 @@ const config: Config = {
       height: createPxValues(2000),
       borderRadius: createPxValues(200),
 
+      screens: {
+        sm: { max: '767px' },
+        md: { min: '768px', max: '1199px' },
+        lg: { min: '1200px' },
+      },
+
       // 기본 값
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         /* Brand gradient */
-        "brand-gradient":
-          "linear-gradient(to right, var(--brand-primary), var(--brand-tertiary))",
+        'brand-gradient':
+          'linear-gradient(to right, var(--brand-primary), var(--brand-tertiary))',
       },
       colors: {
         /* Base colors */
-        black: "var(--black)",
-        white: "var(--white)",
+        black: 'var(--black)',
+        white: 'var(--white)',
 
         /* Brand colors */
-        "brand-primary": "var(--brand-primary)",
-        "brand-secondary": "var(--brand-secondary)",
-        "brand-tertiary": "var(--brand-tertiary)",
+        'brand-primary': 'var(--brand-primary)',
+        'brand-secondary': 'var(--brand-secondary)',
+        'brand-tertiary': 'var(--brand-tertiary)',
 
         /* Point colors */
-        "point-purple": "var(--point-purple)",
-        "point-blue": "var(--point-blue)",
-        "point-cyan": "var(--point-cyan)",
-        "point-pink": "var(--point-pink)",
-        "point-rose": "var(--point-rose)",
-        "point-orange": "var(--point-orange)",
-        "point-yellow": "var(--point-yellow)",
+        'point-purple': 'var(--point-purple)',
+        'point-blue': 'var(--point-blue)',
+        'point-cyan': 'var(--point-cyan)',
+        'point-pink': 'var(--point-pink)',
+        'point-rose': 'var(--point-rose)',
+        'point-orange': 'var(--point-orange)',
+        'point-yellow': 'var(--point-yellow)',
 
         /* Background colors */
-        "bg-primary": "var(--bg-primary)",
-        "bg-secondary": "var(--bg-secondary)",
-        "bg-tertiary": "var(--bg-tertiary)",
-        "bg-inverse": "var(--bg-inverse)",
+        'bg-primary': 'var(--bg-primary)',
+        'bg-secondary': 'var(--bg-secondary)',
+        'bg-tertiary': 'var(--bg-tertiary)',
+        'bg-inverse': 'var(--bg-inverse)',
 
         /* Interaction colors */
-        "it-inactive": "var(--it-inactive)",
-        "it-hover": "var(--it-hover)",
-        "it-pressed": "var(--it-pressed)",
-        "it-focus": "var(--it-focus)",
+        'it-inactive': 'var(--it-inactive)',
+        'it-hover': 'var(--it-hover)',
+        'it-pressed': 'var(--it-pressed)',
+        'it-focus': 'var(--it-focus)',
 
         /* Border color */
-        "bd-primary": "var(--bd-primary)",
+        'bd-primary': 'var(--bd-primary)',
 
         /* Text colors */
-        "text-primary": "var(--text-primary)",
-        "text-secondary": "var(--text-secondary)",
-        "text-tertiary": "var(--text-tertiary)",
-        "text-default": "var(--text-default)",
-        "text-inverse": "var(--text-inverse)",
-        "text-disabled": "var(--text-disabled)",
+        'text-primary': 'var(--text-primary)',
+        'text-secondary': 'var(--text-secondary)',
+        'text-tertiary': 'var(--text-tertiary)',
+        'text-default': 'var(--text-default)',
+        'text-inverse': 'var(--text-inverse)',
+        'text-disabled': 'var(--text-disabled)',
 
         /* Status color */
-        "status-danger": "var(--status-danger)",
+        'status-danger': 'var(--status-danger)',
 
         /* Icon colors */
-        "icon-primary": "var(--icon-primary)",
-        "icon-secondary": "var(--icon-secondary)",
-        "icon-tertiary": "var(--icon-tertiary)",
+        'icon-primary': 'var(--icon-primary)',
+        'icon-secondary': 'var(--icon-secondary)',
+        'icon-tertiary': 'var(--icon-tertiary)',
       },
     },
   },
