19665cd89df909a27cde4f4db5fd150cb1f89a6a
Oh_hong <jungi9933@naver.com>
2024-09-03 14:09:21 +0900

    :fire: 불필요한 코드 삭제

--- components/common/button/Button.tsx
+++ components/common/button/Button.tsx
@@ -9,21 +9,21 @@ interface ButtonProps {
 
 const itemVariants = {
   largeGreen:
-    'flex-center rounded-12 bg-brand-primary mt-100 text-text-inverse hover:bg-it-hover active:bg-it-pressed',
+    'flex-center rounded-12 bg-brand-primary text-text-inverse hover:bg-it-hover active:bg-it-pressed',
   largeWhite:
-    'flex-center rounded-12 bg-white mt-20 text-brand-primary border-1 border-solid border-brand-primary hover:border-it-hover hover:text-it-hover active:border-it-pressed active:text-it-pressed',
+    'flex-center rounded-12 bg-white text-brand-primary border-1 border-solid border-brand-primary hover:border-it-hover hover:text-it-hover active:border-it-pressed active:text-it-pressed',
   largeWhiteSecond:
-    'flex-center rounded-12 bg-white mt-20 text-text-default border-1 border-solid border-text-secondary',
+    'flex-center rounded-12 bg-white text-text-default border-1 border-solid border-text-secondary',
   largeWhiteSecondDanger:
-    'flex-center rounded-12 bg-status-danger mt-20 text-text-inverse',
+    'flex-center rounded-12 bg-status-danger text-text-inverse',
   smallGreen:
-    'flex-center rounded-12 bg-brand-primary mt-20 text-white hover:bg-it-hover active:bg-it-pressed',
+    'flex-center rounded-12 bg-brand-primary text-white hover:bg-it-hover active:bg-it-pressed',
   smallNoneBg:
-    'flex-center rounded-12 mt-20 text-brand-primary border-1 border-solid border-brand-primary hover:border-it-hover hover:text-it-hover active:border-it-pressed active:text-it-pressed',
+    'flex-center rounded-12 text-brand-primary border-1 border-solid border-brand-primary hover:border-it-hover hover:text-it-hover active:border-it-pressed active:text-it-pressed',
   floatingGreen:
-    'flex-center rounded-40 mt-20 bg-brand-primary text-white shadow-floating hover:bg-it-hover active:bg-it-pressed',
+    'flex-center rounded-40 bg-brand-primary text-white shadow-floating hover:bg-it-hover active:bg-it-pressed',
   floatingWhite:
-    'flex-center rounded-40 mt-20 bg-bg-inverse text-brand-primary borer-1 border-solide border-brand-primary shadow-floating hover:border-it-hover hover:text-it-hover active:border-it-pressed active:text-it-pressed',
+    'flex-center rounded-40 bg-bg-inverse text-brand-primary borer-1 border-solide border-brand-primary shadow-floating hover:border-it-hover hover:text-it-hover active:border-it-pressed active:text-it-pressed',
 };
 
 const heightClass = (height: number) => `h-${height}`;
