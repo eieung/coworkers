6723a023f0ee51cc7df47be62f13d1d237c00563
binyongbin <kyb2475@gmail.com>
2024-09-04 22:06:00 +0900

    feat: 헤더 List 컴포넌트 구현

---
+++ components/common/header/List.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import toggleIcon from '@/assets/image/icon/header-toggle.svg';
+import { useState } from 'react';
+import TeamList from './TeamList';
+
+export default function List() {
+  const [isTeamListVisible, setIsTeamListVisible] = useState(false);
+
+  const toggleTeamListVisibility = () => {
+    setIsTeamListVisible((prevState) => !prevState);
+  };
+
+  return (
+    <div className="relative flex gap-x-8 sm:hidden">
+      <div className="flex gap-x-[11px]">
+        <Link href="/team">
+          <span className="font-medium-16 text-text-primary">경영관리팀</span>
+        </Link>
+        <button onClick={toggleTeamListVisibility}>
+          <Image src={toggleIcon} alt="토글" width={16} height={16} />
+        </button>
+      </div>
+      <Link href="/boards">
+        <span className="font-medium-16 text-text-primary">자유게시판</span>
+      </Link>
+      {isTeamListVisible && (
+        <div className="team-list-translate">
+          <TeamList />
+        </div>
+      )}
+    </div>
+  );
+}
