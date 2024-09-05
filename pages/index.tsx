6d7cd3889961cadcad4510affe1b19d925cbf751
Oh_hong <jungi9933@naver.com>
2024-09-05 21:17:34 +0900

    :fire: 불필요한 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
 import Button from '@/components/common/button';
 
 export default function Home() {
-  return (
-    <>
-      <Button
-        size="floating-large"
-        appearance="floating-outlined"
-        fullWidth={false}
-        children="+ 할 일 추가"
-        className=""
-        disabled={true}
-      />
-    </>
-  );
+  return <></>;
 }
