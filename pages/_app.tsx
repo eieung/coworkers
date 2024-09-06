6e337d2d7f971a68a8070fdb76c778e8c9b21c0a
서미영 <seomiyoung3998@gmail.com>
2024-09-06 21:28:17 +0900

    :sparkles: feat: toast 관련 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
-import '@/styles/globals.css';
 import Header from '@/components/common/header';
+import ModalRenderer from '@/components/common/modal/ModalRender';
+import '@/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import { toastContainerProps } from '@/constants/Toast';
+
+const DynamicToastContainer = dynamic(
+  () => import('react-toastify').then((mod) => mod.ToastContainer),
+  { ssr: false },
+);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      <ModalRenderer />
+      <DynamicToastContainer {...toastContainerProps} />
     </>
   );
 }
