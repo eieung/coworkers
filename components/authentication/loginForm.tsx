bf26f1adc6bced2b22d0a37fc28597a67655b0d6
Oh_hong <jungi9933@naver.com>
2024-09-06 17:47:38 +0900

    test 임시 commit

--- components/authentication/loginForm.tsx
+++ components/authentication/loginForm.tsx
@@ -1,20 +1,23 @@
 export default function LoginForm() {
   return (
-    <form className="mb-25 flex flex-col gap-40 md:mb-49 lg:mb-48">
+    <form className="md:mb-49 mb-[25px] flex flex-col gap-[40px] lg:mb-[48px]">
       <div>
-        <div className="flex flex-col gap-24">
-          <div className="flex flex-col gap-12">
-            <label htmlFor="email" className="font-medium-16 text-text-primary">
+        <div className="flex flex-col gap-[24px]">
+          <div className="flex flex-col gap-[12px]">
+            <label
+              htmlFor="email"
+              className="font-medium-[16px] text-text-primary"
+            >
               이메일
             </label>
             <input
               type="email"
               id="email"
               placeholder="이메일을 입력하세요."
-              className="py-13.5 font-regular-14 h-44 w-343 rounded-12 border-1 border-solid border-bd-primary bg-bg-secondary px-16 placeholder-text-default"
+              className="font-regular-14 h-[44px] w-[343px] rounded-[12px] border-[1px] border-solid border-bd-primary bg-bg-secondary px-[16px] py-[13.5px] placeholder-text-default"
             />
           </div>
-          <div className="flex flex-col gap-12">
+          <div className="flex flex-col gap-[12px]">
             <label
               htmlFor="password"
               className="font-medium-16 text-text-primary"
@@ -25,7 +28,7 @@ export default function LoginForm() {
               type="password"
               id="password"
               placeholder="비밀번호를 입력해주세요."
-              className="py-13.5 font-regular-14 h-44 w-343 rounded-12 border-1 border-solid border-bd-primary bg-bg-secondary px-16 placeholder-text-default"
+              className="font-regular-14 h-[44px] w-[343px] rounded-[12px] border-[1px] border-solid border-bd-primary bg-bg-secondary px-[16px] py-[13.5px] placeholder-text-default"
             />
             <div className="flex justify-end text-emerald-500">
               비밀번호를 잊으셨나요?
@@ -33,9 +36,9 @@ export default function LoginForm() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-24">
+      <div className="flex flex-col gap-[24px]">
         <button className="bg-white">로그인</button>
-        <div className="flex-center justify-between gap-12">
+        <div className="flex-center justify-between gap-[12px]">
           <span className="font-medium-16 text-text-primary">
             아직 계정이 없으신가요?
           </span>
--- components/authentication/socialLogin.tsx
+++ components/authentication/socialLogin.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image';
 
 export default function SocialLogin() {
   return (
-    <div className="flex w-full flex-col gap-16">
-      <div className="relative flex items-center gap-24">
+    <div className="flex w-full flex-col gap-[16px]">
+      <div className="relative flex items-center gap-[24px]">
         <div className="flex-grow border-t border-bd-primary"></div>
         <span className="font-medium-16 text-white">OR</span>
         <div className="flex-grow border-t border-bd-primary"></div>
       </div>
       <div className="flex items-center justify-between">
         <span className="font-medium-16 text-white">간편 로그인하기</span>
-        <div className="flex justify-between gap-16">
+        <div className="flex justify-between gap-[16px]">
           <button>
             <Image src={kakaotalk} alt="카카오 로그인" />
           </button>
