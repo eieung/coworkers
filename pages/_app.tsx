b2f8988a3e04acb85b58888044b3dd75a4539787
서미영 <seomiyoung3998@gmail.com>
2024-09-10 17:41:00 +0900

    💄style: _app 파일에 기본 좌우 여백 설정(랜딩페이지 제외)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 import { toastContainerProps } from '@/constants/toast';
 
 const DynamicToastContainer = dynamic(
@@ -12,10 +13,18 @@ const DynamicToastContainer = dynamic(
 );
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const excludePage = router.pathname === '/';
+  const mainClassName = excludePage
+    ? ''
+    : 'm-auto max-w-[1200px] px-4 md:px-6 lg:px-0';
+
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <main className={mainClassName}>
+        <Component {...pageProps} />
+      </main>
       <ModalRenderer />
       <DynamicToastContainer {...toastContainerProps} />
     </>
