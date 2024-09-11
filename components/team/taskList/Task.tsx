5d907bf625c698b1108e25e9336f587643519321
binyongbin <kyb2475@gmail.com>
2024-09-11 16:06:38 +0900

    feat: Task, TaskList에 mockData 연동

--- components/team/taskList/Task.tsx
+++ components/team/taskList/Task.tsx
@@ -2,23 +2,49 @@ import Image from 'next/image';
 import kebabIcon from '@/assets/image/icon/kebab.svg';
 import ProgressBar from '@/components/common/progressBar';
 
-export default function Task() {
+interface TaskProps {
+  task: {
+    name: string;
+    doneBy: {
+      user: {
+        image: string;
+        nickname: string;
+      };
+    };
+    writer: {
+      user: {
+        image: string;
+        nickname: string;
+      };
+    };
+    commentCount: number;
+    description: string;
+    doneTasks: number;
+    totalTasks: number;
+  };
+}
+export default function Task({ task }: TaskProps) {
+    // 진행 상황 비율을 계산
+  const percentage = Math.round((task.doneTasks / task.totalTasks) * 100);
+
   return (
-    <div className="flex flex-row items-center justify-between rounded-xl bg-bg-secondary mb-4">
+    <div className="mb-4 flex flex-row items-center justify-between rounded-xl bg-bg-secondary">
       <div className="flex flex-row items-center gap-x-3">
         <div className="h-10 w-4 rounded-l-xl bg-yellow-400"></div>
-        <b className="font-medium-14 text-text-primary">법인 설립</b>
+        <b className="font-medium-14 text-text-primary">{task.name}</b>
       </div>
       <div className="flex flex-row gap-x-1 pr-2">
         <div className="flex items-center gap-1 rounded-xl bg-bg-primary px-2">
           <div className="h-3 w-3">
             <ProgressBar
-              percentage={75}
+              percentage={percentage}
               pathColor="#10b981"
               trailColor="#f8fafc"
             />
           </div>
-          <span className="font-14-regular text-brand-primary">3/5</span>
+          <span className="font-14-regular text-brand-primary">
+            {task.doneTasks}/{task.totalTasks}
+          </span>
         </div>
         <Image src={kebabIcon} alt="케밥 아이콘" width={16} height={16} />
       </div>
--- components/team/taskList/index.tsx
+++ components/team/taskList/index.tsx
@@ -1,21 +1,25 @@
 import Task from './Task';
+import taskListData from '@/data/taskListMockData';
 
 export default function TaskList() {
+  const { tasks } = taskListData;
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between py-4 pb-4 pt-6">
         <div className="flex gap-x-2">
           <b className="font-medium-16 text-text-primary">할 일 목록</b>
-          <span className="font-regular-16 text-text-default">(4개)</span>
+          <span className="font-regular-16 text-text-default">
+            ({tasks.length}개)
+          </span>
         </div>
         <button className="font-regular-14 text-brand-primary">
           + 새로운 목록 추가하기
         </button>
       </div>
-      <Task />
-      <Task />
-      <Task />
-      <Task />
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
     </div>
   );
 }
