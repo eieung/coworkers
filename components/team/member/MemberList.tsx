aab79d91f417d5dfa87f530a98c4e3ec080c038a
binyongbin <kyb2475@gmail.com>
2024-09-11 16:07:00 +0900

    feat: Member, MemberList에 mockData 연동

--- components/team/member/MemberList.tsx
+++ components/team/member/MemberList.tsx
@@ -2,15 +2,31 @@ import Image from 'next/image';
 import memberIcon from '@/assets/image/icon/member.svg';
 import kebabIcon from '@/assets/image/icon/kebab.svg';
 
-export default function MemberList() {
+interface MemberProps {
+  member: {
+    userImage: string;
+    userEmail: string;
+    userName: string;
+  };
+}
+
+export default function MemberList({ member }: MemberProps) {
   return (
     <div className="flex h-[68px] w-full max-w-[384px] items-center justify-between rounded-2xl bg-bg-secondary px-6 py-5 md:h-[73px] lg:h-[73px]">
       <div className="flex min-w-0 flex-1 items-center gap-x-3">
-        <Image src={memberIcon} alt="멤버" width={32} height={32} />
+        <img
+          src={member.userImage || memberIcon}
+          alt="유저 이미지"
+          width={32}
+          height={32}
+          className='rounded-full'
+        />
         <div className="flex min-w-0 flex-col">
-          <span className="font-medium-14 text-text-primary">김슬아</span>
+          <span className="font-medium-14 text-text-primary">
+            {member.userName}
+          </span>
           <span className="font-regular-12 truncate text-text-secondary">
-            seula123123@codeit.com
+            {member.userEmail}
           </span>
         </div>
       </div>
--- components/team/member/index.tsx
+++ components/team/member/index.tsx
@@ -1,24 +1,26 @@
 import MemberList from './MemberList';
+import groupData from '@/data/groupMockData';
 
 export default function Member() {
+  const { members } = groupData;
+
   return (
     <div className="mt-12">
       <div className="flex items-center justify-between">
         <div className="flex gap-x-2">
           <strong className="font-medium-16 text-text-primary">멤버</strong>
-          <span className="font-regular-16 text-text-default">(6명)</span>
+          <span className="font-regular-16 text-text-default">
+            ({members.length}명)
+          </span>
         </div>
         <button className="font-regular-14 text-brand-primary">
           + 새로운 멤버 초대하기
         </button>
       </div>
       <div className="mt-6 grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-3">
-        <MemberList />
-        <MemberList />
-        <MemberList />
-        <MemberList />
-        <MemberList />
-        <MemberList />
+        {members.map((member) => (
+          <MemberList key={member.userId} member={member} />
+        ))}
       </div>
     </div>
   );
