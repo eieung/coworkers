b92488926391d8709e778ec552a692964555cbcc
Oh_hong <jungi9933@naver.com>
2024-09-13 17:37:28 +0900

    :sparkles: 로그인 페이지 퍼블리싱

--- components/authentication/loginForm.tsx
+++ components/authentication/loginForm.tsx
@@ -1,80 +1,6 @@
-// import Input from '../common/Input';
-// import { useForm } from 'react-hook-form';
-
-// export default function LoginForm() {
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm();
-
-//   const onSubmit = (data: any) => {
-//     console.log(data);
-//   };
-
-//   return (
-//     <form
-//       onSubmit={handleSubmit(onSubmit)}
-//       className="md:mb-49 mb-[25px] flex flex-col gap-10 lg:mb-12"
-//     >
-//       <div>
-//         <div className="flex flex-col gap-6">
-//           <div className="flex flex-col gap-3">
-//             {/* <label
-//                 htmlFor="email"
-//                 className="font-medium-[16px] text-text-primary"
-//               >
-//                 이메일
-//               </label>
-//               <input
-//                 type="email"
-//                 id="email"
-//                 placeholder="이메일을 입력하세요."
-//                 className="font-regular-14 h-11 w-[343px] rounded-[12px] border border-solid border-bd-primary bg-bg-secondary px-4 py-[13.5px] placeholder-text-default"
-//               /> */}
-//             <Input
-//               label="이메일"
-//               type="email"
-//               placeholder="이메일을 입력하세요."
-//               className="h-11 w-full"
-//             />
-
-//           </div>
-//           <div className="flex flex-col gap-3">
-//             <label
-//               htmlFor="password"
-//               className="font-medium-16 text-text-primary"
-//             >
-//               비밀번호
-//             </label>
-//             <input
-//               type="password"
-//               id="password"
-//               placeholder="비밀번호를 입력해주세요."
-//               className="font-regular-14 h-11 w-[343px] rounded-[12px] border border-solid border-bd-primary bg-bg-secondary px-4 py-[13.5px] placeholder-text-default"
-//             />
-//             <div className="flex justify-end text-emerald-500">
-//               비밀번호를 잊으셨나요?
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="flex flex-col gap-6">
-//         <button className="bg-white">로그인</button>
-//         <div className="flex-center justify-between gap-3">
-//           <span className="font-medium-16 text-text-primary">
-//             아직 계정이 없으신가요?
-//           </span>
-//           <span className="font-medium-16 text-emerald-500">가입하기</span>
-//         </div>
-//       </div>
-//     </form>
-//   );
-// }
-// LoginForm.tsx
-// LoginForm.tsx
 import Input from '../common/Input';
 import { useValidation } from '@/utils/InputValidation';
+import Button from '../common/button';
 
 export default function LoginForm() {
   const {
@@ -90,9 +16,14 @@ export default function LoginForm() {
     getPasswordValidationMessage,
   } = useValidation();
 
+  const isFormValid =
+    isEmailValid && isPasswordValid && email !== '' && password !== '';
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    {
+      /*TODO: 로그인 폼 제출 구현*/
+    }
     e.preventDefault();
-    // 폼 제출 로직 추가
   };
 
   return (
@@ -130,9 +61,17 @@ export default function LoginForm() {
         </div>
       </div>
       <div className="flex flex-col gap-6">
-        <button className="bg-white" type="submit">
+        {/* <button className="bg-white" type="submit">
           로그인
-        </button>
+        </button> */}
+        <Button
+          size="large"
+          font="font-16-semibold-16"
+          appearance="solid"
+          fullWidth={true}
+          children="로그인"
+          disabled={!isFormValid}
+        />
         <div className="flex-center justify-between gap-3">
           <span className="font-medium-16 text-text-primary">
             아직 계정이 없으신가요?
