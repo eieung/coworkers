28dc518d1b9730ac5a171621790b8ef4805e4892
binyongbin <kyb2475@gmail.com>
2024-09-30 13:38:06 +0900

    feat: useGroup 훅을 이용한 API 연동

--- components/team/TeamSetting.tsx
+++ components/team/TeamSetting.tsx
@@ -1,13 +1,20 @@
 import Image from 'next/image';
 import settingIcon from '@/assets/image/icon/gear.svg';
 import thumbnailTeam from '@/assets/image/task/thumbnail-team.svg';
-import { GroupResponse } from '@/types/group';
+import { useGroup } from '@/hooks/useGroup';
 
 interface TeamSettingProps {
-  groupData: GroupResponse;
+  groupId: number;
 }
 
-export default function TeamSetting({groupData}: TeamSettingProps) {
+export default function TeamSetting({ groupId }: TeamSettingProps) {
+  const { data: groupData, isLoading, error } = useGroup(groupId);
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
+  // TODO: 이 부분 어떻게 처리하면 좋을 지?
+  if (!groupData) return <div>데이터 없음</div>;
+
   return (
     <div className="relative flex h-16 items-center justify-between rounded-xl bg-bg-secondary px-6 py-5">
       <strong className="font-bold-20 text-white">{groupData.name}</strong>
