e9bf686a4ee95ccdcfffbf2c51d29526b193115d
binyongbin <kyb2475@gmail.com>
2024-10-01 17:46:09 +0900

    feat: 팀 수정 및 삭제 모달 추가

--- components/team/TeamSetting.tsx
+++ components/team/TeamSetting.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image';
 import settingIcon from '@/assets/image/icon/gear.svg';
 import thumbnailTeam from '@/assets/image/task/thumbnail-team.svg';
 import { useGroup } from '@/hooks/useGroup';
+import Dropdown from '@/components/common/dropdown/Dropdown';
+import TeamForm from '@/components/common/modal/TeamForm';
+import ConfirmModal from '@/components/common/modal/ConfirmModal';
+import useModalStore from '@/store/useModalStore';
+import { toast } from 'react-toastify';
+import { useDeleteGroup } from '@/hooks/useDeleteGroup';
 
 interface TeamSettingProps {
   groupId: number;
@@ -9,11 +15,41 @@ interface TeamSettingProps {
 
 export default function TeamSetting({ groupId }: TeamSettingProps) {
   const { data: groupData, isLoading, error } = useGroup(groupId);
+  const openModal = useModalStore((state) => state.openModal);
+  const deleteGroupMutation = useDeleteGroup();
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
   // TODO: 이 부분 어떻게 처리하면 좋을 지?
   if (!groupData) return <div>데이터 없음</div>;
+
+  const handleEditTeam = () => {
+    openModal((close) => (
+      <TeamForm
+        close={close}
+        name={groupData?.name}
+        image={groupData?.image}
+        isEditMode={true}
+        groupId={groupId}
+      />
+    ));
+  };
+
+  const handleDeleteTeam = () => {
+    openModal((close) => (
+      <ConfirmModal
+        title="팀을 삭제하시겠어요?"
+        description="삭제시 복구가 불가합니다. 정말 삭제하시겠어요?"
+        close={close}
+        confirmText="삭제하기"
+        onConfirm={() => {
+          deleteGroupMutation.mutate(groupId);
+          close();
+        }}
+        buttonType="danger"
+      />
+    ));
+  };
 
   return (
     <div className="relative flex h-16 items-center justify-between rounded-xl bg-bg-secondary px-6 py-5">
@@ -25,10 +61,15 @@ export default function TeamSetting({ groupId }: TeamSettingProps) {
         height={64}
         className="absolute right-20 top-0"
       />
-      {/* TODO: 버튼 누르면 DropDown 나오게 해야 함 */}
-      <button>
-        <Image src={settingIcon} alt="설정" width={24} height={24} />
-      </button>
+      <Dropdown
+        trigger={
+          <Image src={settingIcon} alt="설정" width={24} height={24} />
+        }
+        items={[
+          { label: '수정하기', onClick: handleEditTeam },
+          { label: '삭제하기', onClick: handleDeleteTeam },
+        ]}
+      />
     </div>
   );
 }
