0d3071e24aec3ea5f3d968686e94882f89582c86
binyongbin <kyb2475@gmail.com>
2024-10-04 12:57:47 +0900

    feat: 멤버 추방 및 정보 볼 수 있는 기능 연동, 역할에 따라 보이는 컴포넌트 다르게 적용

--- components/team/member/MemberList.tsx
+++ components/team/member/MemberList.tsx
@@ -3,12 +3,82 @@ import memberIcon from '@/assets/image/icon/member.svg';
 import kebabIcon from '@/assets/image/icon/kebab.svg';
 import crownIcon from '@/assets/image/icon/crown.svg';
 import { Member } from '@/types/group';
+import kickUserIcon from '@/assets/image/icon/kick-user.svg';
+import userInfoIcon from '@/assets/image/icon/info.svg';
+import { useEffect, useRef, useState } from 'react';
+import useModalStore from '@/store/useModalStore';
+import CopyEmail from '@/components/common/modal/CopyEmail';
+import ConfirmModal from '@/components/common/modal/ConfirmModal';
+import { toast } from 'react-toastify';
+import { useDeleteMember } from '@/hooks/useDeleteMember';
+import { useUserStore } from '@/store/authStore';
 
 interface MemberProps {
   member: Member;
+  isAdmin: boolean;
 }
 
-export default function MemberList({ member }: MemberProps) {
+export default function MemberList({ member, isAdmin }: MemberProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const openModal = useModalStore((state) => state.openModal);
+
+  const deleteMemberMutation = useDeleteMember(member.groupId);
+
+  const { user } = useUserStore();
+  const currentUserId = user?.id;
+
+  const handleKebabClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const handleOpenUserInfoModal = () => {
+    openModal((close) => (
+      <CopyEmail
+        close={close}
+        userImage={member.userImage}
+        userName={member.userName}
+        userEmail={member.userEmail}
+      />
+    ));
+  };
+
+  const handleKickUserModal = () => {
+    if (member.userId === currentUserId) {
+      toast.error('자기 자신을 추방할 수 없습니다.');
+      return;
+    }
+
+    openModal((close) => (
+      <ConfirmModal
+        title="멤버 추방하기"
+        description={'멤버를 추방합니다.\n정말 추방하시겠어요?'}
+        close={close}
+        isAlert={true}
+        confirmText="추방하기"
+        onConfirm={() => {
+          deleteMemberMutation.mutate(member.userId);
+          close();
+        }}
+        buttonType="danger"
+      />
+    ));
+  };
+
+  // 바깥 여백 클릭 시 메뉴 닫히는 거 자주 쓰이는 데 훅으로 빼도 될 듯
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="flex h-[68px] w-full max-w-[384px] items-center justify-between rounded-2xl bg-bg-secondary px-6 py-5 md:h-[73px] lg:h-[73px]">
       <div className="flex min-w-0 flex-1 items-center gap-x-3">
@@ -31,9 +101,28 @@ export default function MemberList({ member }: MemberProps) {
           </span>
         </div>
       </div>
-      <button className="flex-shrink-0">
-        <Image src={kebabIcon} alt="메뉴" width={16} height={16} />
-      </button>
+      {isMenuOpen ? (
+        <div className="flex items-center justify-center gap-x-1" ref={menuRef}>
+          <button onClick={handleOpenUserInfoModal}>
+            <Image src={userInfoIcon} alt="정보" width={16} height={16} />
+          </button>
+          {isAdmin && (
+            <button onClick={handleKickUserModal}>
+              <Image
+                src={kickUserIcon}
+                alt="추방"
+                width={16}
+                height={16}
+                className="mt-[2px]"
+              />
+            </button>
+          )}
+        </div>
+      ) : (
+        <button className="flex-shrink-0" onClick={handleKebabClick}>
+          <Image src={kebabIcon} alt="메뉴" width={16} height={16} />
+        </button>
+      )}
     </div>
   );
 }
