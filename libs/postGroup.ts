dd57de45fff8b69826129f36c090304126978ba5
binyongbin <kyb2475@gmail.com>
2024-10-04 13:07:02 +0900

    chore: 그룹을 생성하는 api 주석 추가

--- libs/postGroup.ts
+++ libs/postGroup.ts
@@ -1,5 +1,10 @@
 import { authAxiosInstance } from './axios';
 
+/**
+ * 그룹을 생성하는 함수
+ * @param id - 추가할 그룹의 ID
+ * @param data - 추가할 그룹의 데이터 (이름, 이미지)
+ */
 export const postGroup = async (data: {
   name: string;
   image: string | null;
