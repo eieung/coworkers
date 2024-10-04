9d4d5dd81906988a17f78548f319215a3aadf330
binyongbin <kyb2475@gmail.com>
2024-10-04 13:09:58 +0900

    feat: 각 그룹마다 서로 다른 notification 컴포넌트 적용될 수 있게 구현 및 mongoDb 스키마 재정의 및 엔드포인트 변경(폴더 구조 변경)

--- pages/groups/[groupId]/index.tsx
+++ pages/groups/[groupId]/index.tsx
@@ -15,6 +15,7 @@ export default function Team() {
   // groupId가 없더라도 훅은 항상 호출되어야 하므로 기본값 0 설정
   // React Hook 규칙때문에, React Hook은 반드시 컴포넌트의 최상위 레벨에서 호출되어야 하며, 조건부로 호출하면 안 됨
   // 근데 이걸 기본값을 설정해서라도 해야 하는 지? 다른 방법은 없는 지..!
+  // TODO: 전체를 감싸는 컴포넌트 만들어서 해결하기
   const numericGroupId = groupId ? parseInt(groupId as string) : 0;
 
   const { accessToken } = useUserStore();
@@ -48,11 +49,15 @@ export default function Team() {
 
   return (
     <div className="py-6">
-      {numericGroupId && <TeamSetting groupId={numericGroupId} />}
-      <Notification isAdmin={isAdmin} />
-      {numericGroupId && <TaskList groupId={numericGroupId} />}
+      {numericGroupId && (
+        <TeamSetting isAdmin={isAdmin} groupId={numericGroupId} />
+      )}
+      <Notification isAdmin={isAdmin} groupId={numericGroupId} />
+      {numericGroupId && (
+        <TaskList isAdmin={isAdmin} groupId={numericGroupId} />
+      )}
       {numericGroupId && <TaskReport groupId={numericGroupId} />}
-      {numericGroupId && <Member groupId={numericGroupId} />}
+      {numericGroupId && <Member isAdmin={isAdmin} groupId={numericGroupId} />}
     </div>
   );
 }
