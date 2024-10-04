d3047511d948ef075cedd79e3d14dfa9f04ed899
binyongbin <kyb2475@gmail.com>
2024-10-04 12:57:02 +0900

    feat: 멤버의 역할에 따라 보이는 컴포넌트 다르게 적용

--- components/team/taskList/Task.tsx
+++ components/team/taskList/Task.tsx
@@ -3,7 +3,12 @@ import Link from 'next/link';
 import clsx from 'clsx';
 import kebabIcon from '@/assets/image/icon/kebab.svg';
 import doneTaskIcon from '@/assets/image/icon/progress.svg';
+import deleteIcon from '@/assets/image/icon/delete.svg';
 import CircularProgressBar from '@/components/common/CircularProgressBar';
+import { useEffect, useRef, useState } from 'react';
+import useModalStore from '@/store/useModalStore';
+import { useDeleteTaskList } from '@/hooks/useDeleteTaskList';
+import ConfirmModal from '@/components/common/modal/ConfirmModal';
 
 interface TaskProps {
   name: string;
@@ -12,6 +17,7 @@ interface TaskProps {
   displayIndex: number;
   groupId: number;
   taskListId: number;
+  isAdmin: boolean;
 }
 
 export default function Task({
@@ -21,7 +27,14 @@ export default function Task({
   displayIndex,
   groupId,
   taskListId,
+  isAdmin,
 }: TaskProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const openModal = useModalStore((state) => state.openModal);
+
+  const deleteTaskListMutation = useDeleteTaskList(groupId);
+
   const percentage =
     totalTasks > 0 ? Math.round((completedTasks / totalTasks) * 100) : 0;
 
@@ -35,11 +48,41 @@ export default function Task({
     'bg-point-yellow',
   ];
 
-  /**
-   * Task 순서에 따른 색상 구분
-   * 수정 필요 함 (displayIndex로 구분하면 안 됨)
-   */
   const bgColorClass = bgColors[displayIndex % bgColors.length];
+
+  const handleKebabClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const handleDeleteTaskListModal = () => {
+    openModal((close) => (
+      <ConfirmModal
+        title="할 일 목록 삭제하기"
+        description={'할 일 목록을 삭제합니다.\n정말 삭제하시겠어요?'}
+        close={close}
+        isAlert={true}
+        confirmText="삭제하기"
+        onConfirm={() => {
+          deleteTaskListMutation.mutate(taskListId);
+          close();
+        }}
+        buttonType="danger"
+      />
+    ));
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="mb-4 flex flex-row items-center justify-between rounded-xl bg-bg-secondary">
@@ -71,8 +114,33 @@ export default function Task({
             {completedTasks}/{totalTasks}
           </span>
         </div>
-
-        <Image src={kebabIcon} alt="케밥 아이콘" width={16} height={16} />
+        {isAdmin ? (
+          <div className="flex items-center" ref={menuRef}>
+            {!isMenuOpen && (
+              <button onClick={handleKebabClick}>
+                <Image
+                  src={kebabIcon}
+                  alt="케밥 아이콘"
+                  width={16}
+                  height={16}
+                />
+              </button>
+            )}
+            {isMenuOpen && (
+              <button className="w-4" onClick={handleDeleteTaskListModal}>
+                <Image
+                  src={deleteIcon}
+                  alt="삭제"
+                  width={12}
+                  height={12}
+                  className="ml-[2px]"
+                />
+              </button>
+            )}
+          </div>
+        ) : (
+          <div className="w-2"></div>
+        )}
       </div>
     </div>
   );
