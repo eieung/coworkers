46c5916142f5cd5c5b2583f2f70a66cf8b87f8d8
binyongbin <kyb2475@gmail.com>
2024-10-06 15:25:21 +0900

    fix: 로그아웃 버튼을 눌러 로그아웃 후 재 로그인 시 그 전에 로그인 됐던 유저 정보가 나타나는 버그 수정

--- components/common/header/Profile.tsx
+++ components/common/header/Profile.tsx
@@ -6,22 +6,43 @@ import { useUser } from '@/hooks/useUser';
 import useModalStore from '@/store/useModalStore';
 import CustomInputModal from '../modal/CustomInputModal';
 import { toast } from 'react-toastify';
+import { useJoinTeamQuery } from '@/queries/group/invitaion';
+import { useAuth } from '@/hooks/useAuth';
 
 export default function Profile() {
-  const { clearUser, accessToken } = useUserStore();
+  const { accessToken } = useUserStore();
+  const { logout } = useAuth();
+
   const router = useRouter();
   const { data: user } = useUser(accessToken);
   const openModal = useModalStore((state) => state.openModal);
 
-  // api 연동 필요
+  const { mutate: joinTeam } = useJoinTeamQuery(user?.id || 0);
+
   const handleCustomInputModal = () => {
     openModal((close) => (
       <CustomInputModal
         close={close}
         title={<div className="font-medium-24 mb-10">팀 참여하기</div>}
         buttonText={'참여하기'}
-        onAction={(data) => {
-          toast.success(`${data} 팀에 참여되었습니다!`);
+        onAction={(teamToken) => {
+          if (user?.email) {
+            joinTeam(
+              { userEmail: user.email, token: teamToken },
+              {
+                onSuccess: () => {
+                  toast.success('팀에 성공적으로 참여되었습니다!');
+                  close();
+                },
+                onError: (error) => {
+                  toast.error('팀 참여에 실패했습니다. 다시 시도해주세요.');
+                  console.error(error);
+                },
+              },
+            );
+          } else {
+            toast.error('로그인 상태를 확인해주세요.');
+          }
         }}
         placeholder={'팀 링크를 입력해주세요.'}
         label={'팀 링크'}
@@ -33,7 +54,7 @@ export default function Profile() {
   };
 
   const handleLogout = () => {
-    clearUser();
+    logout();
     router.push('/login');
   };
 
