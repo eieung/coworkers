6790993d8d7ad9099499c56743ff363f7fd544bb
binyongbin <kyb2475@gmail.com>
2024-10-09 01:10:26 +0900

    refactor: TaskReport 반응형에 따라 컴포넌트 처 및 주석 제거

--- components/team/taskReport/index.tsx
+++ components/team/taskReport/index.tsx
@@ -11,7 +11,6 @@ export default function TaskReport() {
   const router = useRouter();
   const { groupId } = router.query;
 
-
   const {
     data: groupResponse,
     isLoading,
@@ -57,7 +56,6 @@ export default function TaskReport() {
    * - 이 훅은 `groupData`, `isLoading`, `error`의 변경 사항에 반응하여 실행됨.
    * - 이 세 가지 중 하나라도 변경되면 다시 실행되어 오늘의 작업 수를 계산하고 퍼센트를 업데이트함.
    *
-   * 09/30 (수정): swagger에서 date 설정 하는 부분으로 접근 가능할 것 같아서 리팩토링 예정.
    */
 
   useEffect(() => {
@@ -70,7 +68,7 @@ export default function TaskReport() {
 
     groupData.taskLists.forEach((taskList) => {
       taskList.tasks.forEach((task) => {
-        if (isSameDay(new Date(task.updatedAt), today)) {
+        if (isSameDay(new Date(task.date), today)) {
           totalTasks++;
           if (task.doneAt !== null) {
             completedTasks++;
@@ -93,18 +91,29 @@ export default function TaskReport() {
   return (
     <div className="mt-8">
       <strong className="font-medium-16 text-text-primary">리포트</strong>
-      <div className="mt-4 flex flex-row items-center gap-x-10 rounded-xl bg-bg-secondary p-6">
-        <div className="flex w-[168px] min-w-[112px] flex-col items-center">
+      <div className="mt-4 flex flex-row items-center justify-between gap-x-10 rounded-xl bg-bg-secondary p-6">
+        <div className="relative block w-[169px] min-w-[100px] md:hidden lg:hidden">
           <CircularProgressBar
             percentage={percentage}
-            useGradient
             trailColor="#334155"
             showText={true}
           />
-          <span className="font-medium-14 mt-3 text-text-primary">
-            오늘의 진척도
-          </span>
         </div>
+
+        <div className="flex flex-row gap-x-16 sm:hidden">
+          <div className="w-[168px]">
+            <CircularProgressBar percentage={percentage} trailColor="#334155" />
+          </div>
+          <div className="flex flex-col justify-center gap-y-1">
+            <p className="font-medium-14 text-text-primary">
+              오늘의
+              <br />
+              진행상황
+            </p>
+            <span className="font-bold-40 text-gradient">{percentage}%</span>
+          </div>
+        </div>
+
         <div className="flex w-[400px] flex-col gap-y-4">
           <div className="flex w-full flex-row justify-between gap-x-3 rounded-xl bg-bg-tertiary p-4">
             <div className="flex flex-col">
