b345a9fc63a74e8c96375d7bec347cd74948bbab
서미영 <seomiyoung3998@gmail.com>
2024-10-10 06:05:28 +0900

    🎨refactor: 계정 설정페이지 수정

--- queries/user/index.ts
+++ queries/user/index.ts
@@ -1,8 +1,9 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { User } from '@/types/user';
 import { useUserStore } from '@/store/authStore';
 import { authAxiosInstance } from '@/services/axios';
 import { queryOptions } from '../config';
+import { toast } from 'react-toastify';
 
 /**
  * 사용자 데이터를 가져오는 함수
@@ -43,4 +44,72 @@ export const useAuthQuery = () => {
     logout,
     accessToken,
   };
+};
+
+/**
+ * 사용자 정보를 업데이트하는 함수
+ * @param userData - 사용자 정보 객체
+ * @returns 업데이트된 사용자 데이터
+ */
+const editUser = (userData: { nickname?: string; image?: string }) => {
+  return authAxiosInstance.patch(`user`, userData);
+};
+
+/**
+ * @useEditUsersMutation
+ * 사용자 정보를 업데이트하는 뮤테이션 훅입니다.
+ * @returns 사용자 정보 업데이트 상태와 메소드
+ */
+export const useEditUsersMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      userData,
+    }: {
+      userData: { nickname?: string; image?: string };
+    }) => editUser(userData),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['user'] });
+      toast.success('사용자 정보가 업데이트되었습니다.');
+    },
+    onError: (error: any) => {
+      const errorMessage =
+        error.response?.data?.message ||
+        '사용자 정보 업데이트 중 오류가 발생했습니다.';
+      toast.error(errorMessage);
+      console.error(error);
+    },
+  });
+};
+
+/**
+ * 사용자 정보를 삭제하는 함수
+ * @returns 삭제된 사용자 데이터
+ */
+const deleteUser = () => {
+  return authAxiosInstance.delete(`user`);
+};
+
+/**
+ * @useDeleteUserMutation
+ * 사용자 정보를 삭제하는 뮤테이션 훅입니다.
+ * @returns 사용자 삭제 상태와 메소드
+ */
+export const useDeleteUserMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => deleteUser(),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['user'] });
+      toast.success('사용자가 삭제되었습니다.');
+    },
+    onError: (error: any) => {
+      const errorMessage =
+        error.response?.data?.message || '사용자 삭제 중 오류가 발생했습니다.';
+      toast.error(errorMessage);
+      console.error(error);
+    },
+  });
 };
